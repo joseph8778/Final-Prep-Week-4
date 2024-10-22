722c92ab6da1432831be38260e70a6944c31c551
Joseph <joseph8778@gmail.com>
2024-10-22 07:23:28 -0500

    converted api user data into html. Added dynamic user data and inserted innerHTML into the user-list div in html

--- index.js
+++ index.js
@@ -3,9 +3,24 @@
 
 
 async function main() {
+    
     const users = await fetch("https://jsonplaceholder.typicode.com/users")
     const usersData = await  users.json();
+    const userListEl = document.querySelector(".user-list")
     console.log(usersData)
+
+
+    
+    userListEl.innerHTML = usersData.map((user) => `<div class="user-card">
+    <div class="user-card__container">
+        <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>${user.website}:</b> <a href="${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+    </div>`
+    ).join('');
+
 }
 
 main();
